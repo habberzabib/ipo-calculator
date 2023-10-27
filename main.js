ef2938246755ddcc3dd9856f9d9a67cd73abe23c
habberzabib <145040111+habberzabib@users.noreply.github.com>
2023-10-27 09:00:50 -0600

    Update main.js

--- main.js
+++ main.js
@@ -4,13 +4,18 @@
 document.getElementById("btn").addEventListener("click", btnClicked);
 
 function btnClicked() {
-  //INPUT
+  //INPUT VARIABLES
+
+  //INITIAL INVESTEMENT
   let P = +document.getElementById("P").value;
+  //RATE OF RETURN
   let r = +document.getElementById("r").value;
+  //NUMBER OF TIMES COMPOUNDED ANUALLY
   let n = +document.getElementById("n").value;
+  //NUMBER OF YEARS OF INVESTEMENT
   let t = +document.getElementById("t").value;
 
-  //PROCESS
+  //PROCESS (FORMULA)
   let A = (P*(1+(r/n))**(n*t))
 
  //OUTPUT
